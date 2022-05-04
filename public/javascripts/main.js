dfc34a68a757d5c3ed2212e62310962f7c57275d
Rol3222 <98867905+Rol3222@users.noreply.github.com>
2022-05-04 10:27:54 -0400

    more pages and buttons working

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,8 +5,39 @@
 //Run once broswer has loaded everything
 window.onload = function () {
 
-//All your Front end code should be here!\
 };
+
+var balance = 0;
+
+document.addEventListener('readystatechange', event => { 
+    // When HTML is ready
+    if (event.target.readyState === "interactive") { 
+
+    }
+    // When windows loaded
+    if (event.target.readyState === "complete") {
+        if (document.URL.includes("mainPage.html")) {
+        userBalance = document.getElementById("balance");
+        userBalance.textContent = "Balance: " + balance;
+        }
+    }
+});
+
+function add100() {
+    balance += 100;
+}
+
+function add1000() {
+    balance += 1000;
+}
+
+function add10000() {
+    balance += 10000;
+}
+
+function addCustom() {
+    balance += document.getElementById("customFund").value;
+}
 
 function login() {
     location.href='./login.html';
@@ -17,6 +48,22 @@ function register() {
 }
 
 function mainPage() {
+    location.href='./mainPage.html';
+}
+
+function portfolioPage() {
+    location.href='./portfolioPage.html';
+}
+
+function investPage() {
+    location.href='./investPage.html';
+}
+
+function fundsPage() {
+    location.href='./fundsPage.html';
+}
+
+function back() {
     location.href='./mainPage.html';
 }
 
