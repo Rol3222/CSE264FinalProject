82a7850f92dc8df17cc808fe1a397c8b08e469d0
Rol3222 <98867905+Rol3222@users.noreply.github.com>
2022-05-03 20:41:27 -0400

    html pages, stylesheets, and javascript

---
+++ public/javascripts/main.js
@@ -0,0 +1,25 @@
+/*
+ * This files holds all the code to for your card game
+ */
+
+//Run once broswer has loaded everything
+window.onload = function () {
+
+//All your Front end code should be here!\
+};
+
+function login() {
+    location.href='./login.html';
+}
+
+function register() {
+    location.href='./register.html';
+}
+
+function mainPage() {
+    location.href='./mainPage.html';
+}
+
+function signout() {
+    location.href='./index.html';
+}
