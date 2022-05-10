7a9fb1b702b950c73180b21144f747619d724fa1
jredrlee <jrl222@lehigh.edu>
2022-05-09 23:57:24 -0400

    connect to microsoft server sql

--- app.js
+++ app.js
@@ -15,6 +15,30 @@ dotenv.config({ path: '.env' });
 
 var app = express();
 
+//connecting with sql server
+var Connection = require('tedious').Connection;  
+var config = {  
+    server: 'DESKTOP-E27E30V\SQLEXPRESS.database.windows.net',  //update me
+    authentication: {
+        type: 'default',
+        options: {
+            userName: 'jared', //update me
+            password: 'lee'  //update me
+        }
+    },
+    options: {
+        // If you are on Microsoft Azure, you need encryption:
+        encrypt: true,
+        database: 'your_database'  //update me
+    }
+};  
+var connection = new Connection(config);  
+connection.on('connect', function(err) {  
+    // If no error, then good to proceed.
+    console.log("Connected");  
+});
+
+connection.connect();
 
 //Connect to MongoDB.
  
